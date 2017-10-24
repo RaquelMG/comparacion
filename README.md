282d977860290f624f7c86d6c371ebe2b6f6df45
RaquelMG <raquel_martinez_303@hotmail.com>
2017-10-24 09:12:05 -0500

    subiendo mi primer readme con imagen

---
+++ README.md
@@ -0,0 +1,4 @@
+## Pseudocodigo
+1.-Crea una funcion que reciba dos arreglos
+2.- Comprobar si los dos arreglos tienen la misma longitud
+  2.1.-
---
+++ app.js
@@ -0,0 +1,9 @@
+var comparacion = function(arreglo1, arreglo2){
+  if(arreglo1.length === arreglo2.length ){
+    document.write("si son iguales");
+
+  }
+}
+array1 =[1,2,3,4,5,6,7,8,9,10];
+array2 = [1,2,3,4,5,6,7,8,9,10];
+comparacion(array1,array2);
